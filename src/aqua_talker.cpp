95124265b871e6df046aa2d28daa8a7e71301683
numatanumanuma <chiat18kapi@icloud.com>
2021-10-11 17:26:56 +0900

    Update talker

--- src/aqua_talker.cpp
+++ src/aqua_talker.cpp
@@ -5,26 +5,110 @@
 
 #include "aqua_talker/sound_player.h"
 
-int status_id;
+int movebase_id;
+
+ros::Time start_t;
+ros::Duration limit_t;
+bool is_loop;
+
+namespace RobotBehaviors {
+    enum State {
+        PENDING,
+        RUNNING,
+        STOPPING,
+        RECOVERY,
+        CONFLICT
+    };
+}
+
+void startTimer(double t) {
+    start_t = ros::Time::now();
+    is_loop = false;
+    if (t < 0){
+        is_loop = true;
+    }
+    limit_t = ros::Duration(t);
+}
+bool checkTimer() {
+    if (is_loop)
+        return false;
+    ros::Duration t = ros::Time::now() - start_t;
+    if (ros::Time::now() - start_t > limit_t) {
+        return true;
+    }else {
+        return false;
+    }
+}
 
 void navStatusCallBack(const actionlib_msgs::GoalStatusArray::ConstPtr &status)
 {
-    status_id = 0;
-    //uint8 PENDING         = 0  
-    //uint8 ACTIVE          = 1 
-    //uint8 PREEMPTED       = 2
-    //uint8 SUCCEEDED       = 3
-    //uint8 ABORTED         = 4
-    //uint8 REJECTED        = 5
-    //uint8 PREEMPTING      = 6
-    //uint8 RECALLING       = 7
-    //uint8 RECALLED        = 8
-    //uint8 LOST            = 9
-
+    movebase_id = 0;
     if (!status->status_list.empty()){
         actionlib_msgs::GoalStatus goalStatus = status->status_list[0];
-        status_id = goalStatus.status;
+        movebase_id = goalStatus.status;
     }
+}
+
+/*
+http://docs.ros.org/en/jade/api/actionlib_msgs/html/msg/GoalStatus.html
+uint8 PENDING         = 0   # The goal has yet to be processed by the action server
+uint8 ACTIVE          = 1   # The goal is currently being processed by the action server
+uint8 PREEMPTED       = 2   # The goal received a cancel request after it started executing
+                            #   and has since completed its execution (Terminal State)
+uint8 SUCCEEDED       = 3   # The goal was achieved successfully by the action server (Terminal State)
+uint8 ABORTED         = 4   # The goal was aborted during execution by the action server due
+                            #    to some failure (Terminal State)
+uint8 REJECTED        = 5   # The goal was rejected by the action server without being processed,
+                            #    because the goal was unattainable or invalid (Terminal State)
+uint8 PREEMPTING      = 6   # The goal received a cancel request after it started executing
+                            #    and has not yet completed execution
+uint8 RECALLING       = 7   # The goal received a cancel request before it started executing,
+                            #    but the action server has not yet confirmed that the goal is canceled
+uint8 RECALLED        = 8   # The goal received a cancel request before it started executing
+                            #    and was successfully cancelled (Terminal State)
+uint8 LOST            = 9   # An action client can determine that a goal is LOST. This should not be
+                            #    sent over the wire by an action server
+*/
+int getStatus(){
+    int status = RobotBehaviors::RUNNING;
+    switch (movebase_id)
+        {
+        case 0: // PENDING
+            status = RobotBehaviors::PENDING;
+            break;
+        case 1: // ACTIVE
+            status = RobotBehaviors::STOPPING;
+            break;
+        case 2: // PREEMPTED
+            status = RobotBehaviors::RUNNING;
+            break;
+        case 3: // SUCCEEDED
+            status = RobotBehaviors::RUNNING;
+            break;
+        case 4: // ABORTED
+            status = RobotBehaviors::STOPPING;
+            break;
+        case 5: // REJECTED
+            status = RobotBehaviors::STOPPING;
+            break;
+        case 6: // PREEMPTING
+            status = RobotBehaviors::STOPPING;
+            break;
+        case 7: // RECALLING
+            status = RobotBehaviors::STOPPING;
+            break;
+        case 8: // RECALLED
+            status = RobotBehaviors::STOPPING;
+            break;
+        case 9: // LOST
+            status = RobotBehaviors::STOPPING;
+            break;
+        default:
+            status = RobotBehaviors::PENDING;
+            break;
+        }
+    std::cout << "Now movebase_id... " << movebase_id << std::endl;
+    return status;
 }
 
 int main(int argc, char **argv)
@@ -43,27 +127,48 @@ int main(int argc, char **argv)
     nh.param("debug", debug, false);
 
     SoundPlayer player;
-    ros::Duration(1).sleep();
+    ros::Duration(2).sleep();
     ros::spinOnce();
 
-    status_id = -1;
-    int pre_status_id = -1;
-    bool is_update = true;
+    int pre_status = -1;
+    bool bgm_playing = false;
     while(ros::ok()) {
-        std::cout << "Now status_id... " << status_id << std::endl;
-        if (status_id != pre_status_id)
-            is_update = true;
-        if(!is_update)
+        int status = getStatus();
+        if (status == pre_status){
+            // 前回と同じstatusだった場合, スルー
+            looprate.sleep();
+            ros::spinOnce();
             continue;
-        
-        switch (status_id)
+        }
+        if (bgm_playing){
+            // play()でbgmをループで流しているときcancelしないと上書きされないときがある
+            player.cancel();
+        }
+        bgm_playing = false;
+
+        switch (status)
         {
-        case 0:
-            std::cout << "hai" << std::endl;
+        case RobotBehaviors::PENDING:
+            player.say("pending");
+            std::cout << "PENDING..." << std::endl;
             break;
+        case RobotBehaviors::RUNNING:
+            player.setSound(sound1);
+            player.play();
+            bgm_playing = true;
+            // player.say("running");
+            std::cout << "RUNNING!" << std::endl;
+            break;
+        case RobotBehaviors::STOPPING:
+            player.setSound(sound2);
+            player.playOnce();
+            // player.say("stopping");
+            std::cout << "STOPPING......" << std::endl; 
+            break;       
         default:
             break;
         }
+        pre_status = status;
 
         looprate.sleep();
         ros::spinOnce();
